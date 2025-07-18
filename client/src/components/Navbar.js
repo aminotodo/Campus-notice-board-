f446e42d3f75c2e243509f7e442375053b3706c8
aminotodo <manjani1830@gmail.com>
2025-07-18 18:17:59 +0530

    Fix favicon and navbar logo path

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 function Navbar(){
   return(
     <nav style={{padding:'10px', backgroundColor:'#1abc9c'}}>
-     <img src="/vscode.png" alt="Logo" width="32" height="32" />
+     <img src="/vslogo.png" alt="logo" width="32" legth="32" />
     <p style={{color:''}}>Go-to</p>
       <Link to="/" style={{marginRight:'20px', color:''}}>Home</Link>
       < Link to="/add">Add Notice</Link>
